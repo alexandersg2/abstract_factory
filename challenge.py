14494d132ab4976db83d0b84445bfd665325821f
Alex Ryan <alex.ryan@inspectorio.com>
2022-07-05 18:26:17 +0700

    Improve naming in challenge code

--- challenge.py
+++ challenge.py
@@ -1,21 +1,21 @@
 from abc import ABC, abstractmethod
 
 
-class FileProcessor(ABC):
+class FileReader(ABC):
 
     @abstractmethod
     def read_file(self, filename, extension):
         ...
 
 
-class CSVFileProcessor(FileProcessor):
+class CSVFileReader(FileReader):
 
     def read_file(self, filename, extension):
         print("Did some CSV reader specific setup")
         print(f"Read the [{filename}.{extension}] CSV file ")
 
 
-class XMLFileProcessor(FileProcessor):
+class XMLFileReader(FileReader):
 
     def read_file(self, filename, extension):
         print("Did some XML reader specific setup")
@@ -25,13 +25,13 @@ class XMLFileProcessor(FileProcessor):
 class FileService(ABC):
 
     @abstractmethod
-    def create_file_processor(self):
+    def create_file_reader(self):
         ...
 
     def read_file(self, filename, extension):
         self.verify_file()
-        file_processor = self.create_file_processor()
-        file_processor.read_file(filename, extension)
+        file_reader = self.create_file_reader()
+        file_reader.read_file(filename, extension)
     
     def verify_file(self):
         print("Verified file exists")
@@ -39,18 +39,18 @@ class FileService(ABC):
 
 class CSVFileService(FileService):
 
-    def create_file_processor(self):
+    def create_file_reader(self):
         print("Did some special CSV file initialization stuff")
-        return CSVFileProcessor()
+        return CSVFileReader()
 
 
 class XMLFileService(FileService):
 
-    def create_file_processor(self):
-        return XMLFileProcessor()
+    def create_file_reader(self):
+        return XMLFileReader()
 
 
-FILE_HANDLER_MAP = {
+FILE_SERVICE_MAP = {
         "xml": XMLFileService,
         "csv": CSVFileService,
     }
@@ -60,7 +60,7 @@ def get_file():
     while True:
         filename = input("Input filename with extension: ").lower()
         filename, extension = filename.split('.')
-        if extension in FILE_HANDLER_MAP.keys():
+        if extension in FILE_SERVICE_MAP.keys():
             return filename, extension
 
 def get_function():
@@ -74,13 +74,13 @@ def main():
     func = get_function()
     filename, extension = get_file()
 
-    file_handler_service = FILE_HANDLER_MAP.get(extension)()
+    file_service = FILE_SERVICE_MAP.get(extension)()
 
     if func == 'r':
-        file_handler_service.read_file(filename, extension)
+        file_service.read_file(filename, extension)
     else:
         # TODO: Write the file
-        ...
+        print("File writing not yet supported")
 
 
 if __name__ == "__main__":
